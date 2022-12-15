79b173af25ede7b4559e7327cfef9aa6277415e1
yuta <eypc.ofun@gmail.com>
2022-12-15 23:30:17 +0900

    add dark mode

--- laravel/resources/views/layouts/template.blade.php
+++ laravel/resources/views/layouts/template.blade.php
@@ -3,11 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="css/common.css"/>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            darkMode: 'class',
+        }
+    </script>
     @yield('stylesheet')
 </head>
-<body>
-@include('common.validation-message')
-@yield('content')
+<body
+    class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-600">
+@include('common.header')
+<div class="w-full mt-6 mx-6 md:mx-auto md:w-4/5 xl:w-2/3">
+    @include('common.validation-message')
+    @yield('content')
+</div>
 </body>
 </html>
