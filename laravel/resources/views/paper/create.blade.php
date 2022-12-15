d2021a130342659f61dea8b67f3937bda268eabc
yuta <eypc.ofun@gmail.com>
2022-12-16 00:44:17 +0900

    fix design

--- laravel/resources/views/paper/create.blade.php
+++ laravel/resources/views/paper/create.blade.php
@@ -1,29 +1,42 @@
 @extends('layouts.template')
 @section('title', '論文作成')
 @section('content')
-    <h1 class="text-lg">Paper Register</h1>
     <form action="{{ route("papers.store") }}" method="POST" enctype="multipart/form-data">
         @csrf
-        <label>
-            Title
-            <input name="title">
-        </label>
+        <div class="mb-6">
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Title
+                <input name="title" type="text"
+                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            </label>
+        </div>
 
-        <label>
-            Memo
-            <input name="memo">
-        </label>
+        <div class="mb-6">
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Memo
+                <input name="memo" type="text"
+                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            </label>
+        </div>
 
-        <label>
-            URL
-            <input name="url">
-        </label>
+        <div class="mb-6">
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                URL
+                <input name="url" type="text"
+                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            </label>
+        </div>
 
-        <label>
-            PDF
-            <input name="pdf" type="file" accept="application/pdf">
-        </label>
+        <div class="mb-6">
+            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                PDF
+                <input name="pdf" type="file" accept="application/pdf">
+            </label>
+        </div>
 
-        <button type="submit">submit</button>
+        <button type="submit"
+                class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800">
+            submit
+        </button>
     </form>
 @endsection
--- laravel/resources/views/paper/edit.blade.php
+++ laravel/resources/views/paper/edit.blade.php
@@ -1,31 +1,56 @@
 @extends('layouts.template')
 @section('title', '論文編集')
 @section('content')
-    <h1 class="text-lg">Paper</h1>
     <form action="{{ route('papers.update', $paper->id) }}" method="POST" enctype="multipart/form-data">
         @method('PUT')
         @csrf
-        <label>
+        <div class="mt-8">
+            <button type="submit"
+                    class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800"
+            >Update
+            </button>
+        </div>
+
+        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
             Title
-            <input name="title" value="{{ $paper->title }}">
+            <input name="title" value="{{ old("title", $paper->title) }}"
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         </label>
 
-        <label>
+        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
             Memo
-            <input name="memo" value="{{ $paper->memo }}">
+            <input name="memo" value="{{ old("memo", $paper->memo) }}"
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         </label>
 
-        <label>
+        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
             URL
-            <input name="url" value="{{ $paper->url }}">
+            <input name="url" value="{{ old("url", $paper->url) }}"
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
         </label>
 
-        <label>
+        <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
             PDF
-            <input name="pdf" type="file" accept="application/pdf">
+            <div>
+                <input name="pdf" type="file" accept="application/pdf">
+            </div>
         </label>
-        <a href="{{ $paper->pdf_url }}" target="_blank">Download</a>
 
-        <button type="submit">更新</button>
+        @if($paper->pdf_url)
+            <div class="mt-3">
+                <a href="{{ $paper->pdf_url }}" target="_blank">
+                    <button type="button"
+                            class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">
+                        PDF Download
+                    </button>
+                </a>
+                <button
+                    class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+                    type="button" data-modal-toggle="defaultModal">
+                    PDF Delete
+                    {{-- TODO: delete --}}
+                </button>
+            </div>
+        @endif
     </form>
 @endsection
